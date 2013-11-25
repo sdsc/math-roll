aef801ed13487d0f6cfee8b1ded721c1a00b4242
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 12:32:53 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/parmetis/version.mk
+++ src/parmetis/version.mk
@@ -1,4 +1,15 @@
-NAME    = parmetis_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION = 4.0.2
-RELEASE = 5
-RPM.EXTRAS = "AutoReq: no"
+NAME               = parmetis_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 4.0.2
+RELEASE            = 5
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/parmetis/$(ROLLCOMPILER)/$(ROLLMPI)/$(ROLLNETWORK)
+
+SRC_SUBDIR         = parmetis
+
+SOURCE_NAME        = parmetis
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
