7d0024e9aa459b3883d8dabd1f0f1aa654746c07
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 13:00:01 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/superlu/version.mk
+++ src/superlu/version.mk
@@ -1,4 +1,16 @@
-NAME    = superlu_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION = 3.2
-RELEASE = 5
-RPM.EXTRAS = "AutoReq: no"
+NAME               = superlu_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 3.2
+RELEASE            = 5
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/superlu/$(ROLLCOMPILER)/$(ROLLMPI)/$(ROLLNETWORK)
+
+SRC_SUBDIR         = superlu
+
+SOURCE_NAME        = superlu
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TGZ_PKGS           = $(SOURCE_PKG)
+
