3a0c5e74ad31a237e80d8b145944a2fc45bde2cc
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 11:57:23 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/eigen/version.mk
+++ src/eigen/version.mk
@@ -1,3 +1,14 @@
-NAME    = eigen_$(ROLLCOMPILER)
-VERSION = 3.1.3
-RELEASE = 0
+NAME               = eigen_$(ROLLCOMPILER)
+VERSION            = 3.1.3
+RELEASE            = 0
+PKGROOT            = /opt/eigen/$(ROLLCOMPILER)
+
+SRC_SUBDIR         = eigen
+
+SOURCE_NAME        = eigen
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
