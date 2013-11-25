b6b9b223a1ba03faa2c75eeea5ebd95cfe26fbb4
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 12:25:35 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/octave/version.mk
+++ src/octave/version.mk
@@ -1,4 +1,15 @@
-NAME    = octave
-VERSION = 3.6.2
-RELEASE = 3
-RPM.EXTRAS = "AutoReq: no"
+NAME               = octave
+VERSION            = 3.6.2
+RELEASE            = 3
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/octave
+
+SRC_SUBDIR         = octave
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
