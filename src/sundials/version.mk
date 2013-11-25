353befdfb91eb5f5045f2e9c4dadda0ec9cad433
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 12:53:53 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/sundials/version.mk
+++ src/sundials/version.mk
@@ -1,4 +1,15 @@
-NAME    = sundials_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION = 2.5.0
-RELEASE = 0
-RPM.EXTRAS = "AutoReq: no"
+NAME               = sundials_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 2.5.0
+RELEASE            = 0
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/sundials/$(ROLLCOMPILER)/$(ROLLMPI)/$(ROLLNETWORK)
+
+SRC_SUBDIR         = sundials
+
+SOURCE_NAME        = sundials
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
