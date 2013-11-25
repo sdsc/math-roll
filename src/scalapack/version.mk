b3be28f75403d44d00e242fd764d21660d70cba2
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 12:45:57 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/scalapack/version.mk
+++ src/scalapack/version.mk
@@ -1,4 +1,16 @@
-NAME    = scalapack_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION = 2.0.2
-RELEASE = 5
-RPM.EXTRAS = "AutoReq: no"
+NAME               = scalapack_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 2.0.2
+RELEASE            = 5
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/scalapack/$(ROLLCOMPILER)/$(ROLLMPI)/$(ROLLNETWORK)
+
+SRC_SUBDIR         = scalapack
+
+SOURCE_NAME        = scalapack
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TGZ_PKGS           = $(SOURCE_PKG)
+
