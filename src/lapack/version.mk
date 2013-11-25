802d9994c510ba6385ad040dbaa47c865f989afc
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 12:16:49 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/lapack/version.mk
+++ src/lapack/version.mk
@@ -1,4 +1,19 @@
-NAME    = lapack_$(ROLLCOMPILER)
-VERSION = 3.4.2
-RELEASE = 0
-RPM.EXTRAS = "AutoReq: no"
+NAME               = lapack_$(ROLLCOMPILER)
+VERSION            = 3.4.2
+RELEASE            = 0
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/lapack/$(ROLLCOMPILER)
+
+SRC_SUBDIR         = lapack
+
+SOURCE_NAME        = lapack
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+BLAS_NAME          = blas
+BLAS_PKG           = $(BLAS_NAME).$(SOURCE_SUFFIX)
+BLAS_DIR           = $(BLAS_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TGZ_PKGS           = $(SOURCE_PKG) $(BLAS_PKG)
