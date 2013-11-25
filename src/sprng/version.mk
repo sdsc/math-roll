7fa8c077b4f5f23d36cddd3d37dc13cbc7efaa0f
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 12:50:10 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/sprng/version.mk
+++ src/sprng/version.mk
@@ -1,4 +1,15 @@
-NAME    = sprng_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION = 2.0b
-RELEASE = 24
-RPM.EXTRAS = "AutoReq: no"
+NAME               = sprng_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 2.0b
+RELEASE            = 24
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/sprng/$(ROLLCOMPILER)/$(ROLLMPI)/$(ROLLNETWORK)
+
+SRC_SUBDIR         = sprng
+
+SOURCE_NAME        = sprng
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TGZ_PKGS           = $(SOURCE_PKG)
