0f9be43b0e22110074be3c535d3187e098869e9f
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 12:06:03 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/gsl/version.mk
+++ src/gsl/version.mk
@@ -1,4 +1,15 @@
-NAME    = gsl_$(ROLLCOMPILER)
-VERSION = 1.15
-RELEASE = 0
-RPM.EXTRAS = "AutoReq: no"
+NAME               = gsl_$(ROLLCOMPILER)
+VERSION            = 1.15
+RELEASE            = 0
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/gsl/$(ROLLCOMPILER)
+
+SRC_SUBDIR         = gsl
+
+SOURCE_NAME        = gsl
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
