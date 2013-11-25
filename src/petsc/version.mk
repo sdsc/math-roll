3f4150fceba1c5a5f860aacfaa0bbed1ff4ecb25
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 12:39:19 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/petsc/version.mk
+++ src/petsc/version.mk
@@ -1,4 +1,15 @@
-NAME    = petsc_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION = 3.3.p5
-RELEASE = 1
-RPM.EXTRAS = "AutoReq: no"
+NAME               = petsc_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 3.3.p5
+RELEASE            = 1
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/petsc/$(ROLLCOMPILER)/$(ROLLMPI)/$(ROLLNETWORK)
+
+SRC_SUBDIR         = petsc
+
+SOURCE_NAME        = petsc
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
