fdedb1df0b8a55f7ec8689c755c7447607ea513f
Jim Hayes <jhayes@sdsc.edu>
2017-11-09 10:04:57 -0800

    Fix paths to MKL libraries.
    Addresses APS-608.

--- src/petsc/version.mk
+++ src/petsc/version.mk
@@ -10,7 +10,7 @@ MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-petsc_$(COMPILERNAME)_$(MPINAME)
 VERSION        = 3.6.3
-RELEASE        = 5
+RELEASE        = 6
 PKGROOT        = /opt/petsc/$(COMPILERNAME)/$(MPINAME)
 
 SRC_SUBDIR     = petsc
