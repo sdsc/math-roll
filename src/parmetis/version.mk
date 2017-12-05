4234341025fac6e3776a652f4c2b445363d765d4
Jim Hayes <jhayes@sdsc.edu>
2017-12-04 17:38:55 -0800

    Add missing creation of target directory.

--- src/parmetis/version.mk
+++ src/parmetis/version.mk
@@ -10,7 +10,7 @@ MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-parmetis_$(COMPILERNAME)_$(MPINAME)
 VERSION        = 4.0.3
-RELEASE        = 5
+RELEASE        = 6
 PKGROOT        = /opt/parmetis/$(COMPILERNAME)/$(MPINAME)
 
 SRC_SUBDIR     = parmetis
