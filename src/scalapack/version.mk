12359e0564b9c587dfa07adae2b96f796274e1f1
Jerry Greenberg <jpg@sdsc.edu>
2018-10-02 13:54:05 -0700

    APS-852 use multithreaded make for scalapack

--- src/scalapack/version.mk
+++ src/scalapack/version.mk
@@ -10,7 +10,7 @@ MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-scalapack_$(COMPILERNAME)_$(MPINAME)
 VERSION        = 2.0.2
-RELEASE        = 5
+RELEASE        = 6
 PKGROOT        = /opt/scalapack/$(COMPILERNAME)/$(MPINAME)
 
 SRC_SUBDIR     = scalapack
