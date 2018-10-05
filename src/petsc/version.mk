c0a9b6a7f1b5db674199375ac3679a742c553402
Jerry Greenberg <jpg@gordon-ln1.sdsc.edu>
2018-10-05 14:46:04 -0700

    APS-852 put in correct version of mumps

--- src/petsc/version.mk
+++ src/petsc/version.mk
@@ -11,7 +11,7 @@ MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 NAME           = sdsc-petsc_$(COMPILERNAME)_$(MPINAME)
 VERSION        = 3.10.1
 RELEASE        = 0
-PKGROOT        = /opt/petsc/$(COMPILERNAME)/$(MPINAME)
+PKGROOT        = /home/jpg/opt/petsc/$(COMPILERNAME)/$(MPINAME)
 
 SRC_SUBDIR     = petsc
 
@@ -23,8 +23,8 @@ SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
 MUMPS_NAME    = petsc-pkg-mumps
 MUMPS_VERSION = 5.1.2
-MUMPS_PKG     = $(MUMPS_NAME)_$(MUMPS_VERSION).$(SOURCE_SUFFIX)
-MUMPS_DIR     = $(MUMPS_PKG:%.$(SOURCE_SUFFIX)=%)
+MUMPS_PKG     = $(MUMPS_NAME)-$(MUMPS_VERSION).$(SOURCE_SUFFIX)
+MUMPS_DIR     = $(MUMPS_PKG:%.$(MUMPS_SUFFIX)=%)
 
 PASTIX_NAME    = pastix
 PASTIX_SUFFIX  = tar.bz2
