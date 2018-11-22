0e1f93856cb873a3eb1bcb45fb5933f2286276d8
Jerry Greenberg <jpg@sdsc.edu>
2018-11-22 13:10:12 -0800

    APS-928 sprng 5.0

--- src/sprng/version.mk
+++ src/sprng/version.mk
@@ -9,19 +9,19 @@ endif
 MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-sprng_$(COMPILERNAME)_$(MPINAME)
-VERSION        = 2.0b
-RELEASE        = 5
+VERSION        = 5
+RELEASE        = 0
 PKGROOT        = /opt/sprng/$(COMPILERNAME)/$(MPINAME)
 
 SRC_SUBDIR     = sprng
 
 SOURCE_NAME    = sprng
-SOURCE_SUFFIX  = tgz
+SOURCE_SUFFIX  = tar.bz2
 SOURCE_VERSION = $(VERSION)
-SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_PKG     = $(SOURCE_NAME)$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
-TGZ_PKGS       = $(SOURCE_PKG)
+TAR_BZ2_PKGS       = $(SOURCE_PKG)
 
 RPM.EXTRAS     = AutoReq:No
 RPM.PREFIX     = $(PKGROOT)
